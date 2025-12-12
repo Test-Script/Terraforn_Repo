70ce9e46702ae624e5098ae6222200b28ddb028b
Tejas Targhale <tejastarghale47@gmail.com>
2025-12-13 02:45:37 +0530

    deleted the varibles from the variable.tf files

--- variables.tf
+++ variables.tf
@@ -1,8 +1,3 @@
-variable "subscription_id" {}
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
-
 variable "prefix" {
   description = "Name prefix for resources"
   type        = string
